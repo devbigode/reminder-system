87eb5e61b30267babc916013536836441e92488a
Guilherme Morais <guilherme.ads@proton.me>
2025-08-25 12:11:44 -0300

    feat(app): usa AudioManager e getConfigValidator

--- src/main/java/org/devbigode/remindersystem/App.java
+++ src/main/java/org/devbigode/remindersystem/App.java
@@ -6,10 +6,11 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.devbigode.remindersystem.model.DefaultValue;
-import org.devbigode.remindersystem.service.ConfigManager;
+import org.devbigode.remindersystem.service.AudioManager;
 import org.devbigode.remindersystem.service.ConfigValidator;
 import org.devbigode.remindersystem.view.NotificationView;
 
+import java.io.File;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -28,26 +29,19 @@ public class App extends Application {
     public void start(Stage stage) {
         setupStage(stage);
 
-        ConfigManager configManager = new ConfigManager("config.properties");
-        configManager.loadConfig();
+        ConfigValidator configValidator = ConfigValidator.getConfigValidator();
 
-        String title = configManager.getProperty("title", DefaultValue.TITLE.getValue());
-        String message = configManager.getProperty("message", DefaultValue.MESSAGE.getValue());
-        String position = configManager.getPosition("position", DefaultValue.POSITION.getValue());
-        int duration = configManager.getInt("duration", DefaultValue.DURATION.getValue());
-        String image = configManager.getProperty("image", DefaultValue.IMAGE.getValue());
-        int interval = configManager.getInt("interval", DefaultValue.INTERVAL.getValue());
-
-        ConfigValidator configValidator = new ConfigValidator(title, message, position, duration, interval, image);
-        configValidator.validateAllValues();
+        AudioManager audioManager = new AudioManager(new File(DefaultValue.AUDIO.getValue()));
 
         NotificationView notificationView = new NotificationView(configValidator);
+
         Runnable showNotificationTask = () -> {
             Platform.runLater(notificationView::showNotification);
+            audioManager.startSound(audioManager.validateAudio());
         };
 
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-        ScheduledFuture<?> scheduledFuture = executorService.scheduleAtFixedRate(showNotificationTask, 1, configValidator.getInterval(), TimeUnit.MINUTES);
+        ScheduledFuture<?> scheduledFuture = executorService.scheduleAtFixedRate(showNotificationTask, 0, configValidator.getInterval(), TimeUnit.MINUTES);
 
         Runnable stopNotifications = () -> {
             scheduledFuture.cancel(false);
