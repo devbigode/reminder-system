f3b3de545f8f3a5feb0d4dff59cb2310ab3f6a52
Guilherme Morais <guilherme.ads@proton.me>
2025-08-25 11:57:59 -0300

    chore: modifica arquivos e configurações

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.devbigode.remindersystem {
     requires javafx.fxml;
     requires java.sql;
     requires org.controlsfx.controls;
+    requires java.desktop;
 
 
     opens org.devbigode.remindersystem to javafx.fxml;
